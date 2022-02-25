1d0a3f3934ef01dd1fcd81bc1dee1d2e5f44232b
math3us2021 <matheusbf.mf@gmail.com>
2022-02-25 20:49:35 -0300

    aula_dsm

--- 01_busca_sequencial.py
+++ 01_busca_sequencial.py
@@ -7,7 +7,9 @@
 # b) -1, se o elemento não existir
 
 nums = [3, 6, 9, 12, 15, 18, 21, 24, 27, 30, 33, 36, 39, 42, 45, 48, 51, 54, 57, 60]
-
+from data.primos import primos
+from data.lista_nomes import nomes
+from time import time
 """
     Função que implementa a busca sequencial
     Procura por val dentro de lista
@@ -18,6 +20,23 @@ def busca_sequencial(val, lista):
         if lista[pos] == val: return pos
     return -1   # Nada encontrado
 
-print(f"Posição de 27: {busca_sequencial(27, nums)}")
-print(f"Posição de 40: {busca_sequencial(40, nums)}")
+# hora_ini = time()
+# print(f"Posição de 1487: {busca_sequencial(27, primos)}")
+# hora_fim = time
+# print(f"Posição de 1487: {(hora_fim - hora_ini)*1000}")
     
+# hora_int = time()
+# print(f"Posição de 7603: {busca_sequencial(7603, primos)}")
+# hora_fim = time
+# print(f"Posição de 7603: {(hora_fim - hora_ini)*1000}")
+
+# hora_int = time()
+# print(f"Posição de 8008: {busca_sequencial(8008, primos)}")
+# hora_fim = time
+# print(f"Posição de 8008: {(hora_fim - hora_ini)*1000}")
+
+
+hora_ini = time()
+print(f"Posicao do nome 'orkutilson' :{busca_sequencial('ORKUTILSON', nomes)}")
+hora_fim = time ()
+print(f"Tempo gasto para buscar ORKUTILSON:{(hora_fim - hora_ini) *1000}ms")
